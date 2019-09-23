470f375f979f39098e496b7e3d5f6eed47b996ba
Mar Prieto <mar.pnieto@gmail.com>
2019-09-23 16:41:18 +0200

    render list

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Item = (props) =>{
     return(
-        <li key={`RM${props.character.id}`} className = "character__list--item">
+        <li key={props.character.id} className = "character__list--item">
             <img className="character__pic" src ={props.character.image} alt= {props.character.name} />
             <p className="character__name"> {props.character.name} </p>
-            <p className="character__species">{props.character.species}</p>
+            <p className="character__species">{props.character.species} </p>
         </li>)
 }
 
