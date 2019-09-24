21d5b5a0134fc6d682f5da8a49d2a94a850826dc
Mar Prieto <mar.pnieto@gmail.com>
2019-09-24 12:24:36 +0200

    Finish docs v1.0.0

---
+++ docs/precache-manifest.f6de0c9681634fa895aca14559632899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d675a28057ba52e578d30d9177222b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7eba1adf97db3c8dcea",
+    "url": "./static/css/main.2bd8ad33.chunk.css"
+  },
+  {
+    "revision": "6090996fffa88c285f33",
+    "url": "./static/js/2.9cd21640.chunk.js"
+  },
+  {
+    "revision": "e7eba1adf97db3c8dcea",
+    "url": "./static/js/main.8d078d6f.chunk.js"
+  },
+  {
+    "revision": "fe461027164d6533c339",
+    "url": "./static/js/runtime-main.144315fd.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty_-_logo.75509f2e.png"
+  }
+]);
